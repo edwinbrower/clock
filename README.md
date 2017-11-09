12e6e2a4f159f93b74295e9eb63719f960693c53
Edwin Brower <edwin.brower@gmail.com>
2017-11-08 23:41:14 -0800

    clock on server

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # clock
 making clocks in a few ways
+
+To run server:
+nodemon clockOnServer/server.js
---
+++ clockOnServer/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(express.static(path.join(__dirname, 'src')));
+
+app.get('/', function(req, res) {
+  res.send('getting');
+});
+
+app.listen(3001, function() {
+  console.log('listening on port 3001!');
+});
